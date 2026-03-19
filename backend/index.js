cccb52f49b01e559ad305fff3aa7c4e7abdd687c
avnitkumar <avnitkumarmuz@gmail.com>
2026-03-19 08:58:10 +0530

    Middleware to handle CORD

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express"
-
+import cors from "cors"
+import dotenv from "dotenv"
 const app = express()
+//Middleware to handle cors
+ app.use(
+    cors({
+        origin: process.env.FRONT_END_URL || "http://localhost:5173",
+        methods:["GET","POST","PUT","DELETE"],
+        allowedHeaders:["Content-Type", "Authorization"],
 
+    })
+ )
+ //Middleware to handle jspn object in req body
+ app.use(express.json())
 app.listen(3000, () => {
     console.log("Server is running on port 3000!")
 })
