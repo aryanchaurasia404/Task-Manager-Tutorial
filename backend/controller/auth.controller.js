38afc45d2160dbf9266f7ec5ca7523250d572b1f
avnitkumar <avnitkumarmuz@gmail.com>
2026-03-20 13:28:34 +0530

    get user profile api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -76,3 +76,18 @@ try {
 }
 
 }
+
+export const userProfile = async ( req , res , next) => {
+    try {
+        const user = await User.findById(req.user.id)
+
+        if(!user){
+            return next(errorHandler(404, "User not found!"))
+        }
+        const{password:pass, ...rest} = user._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+        
+    }
+}
