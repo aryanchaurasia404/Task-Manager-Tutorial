de5c6d6ccbf8db43f68a60cb20c0712779842aed
avnitkumar <avnitkumarmuz@gmail.com>
2026-03-23 12:25:21 +0530

    creating report generation api route

--- backend/routes/report.route.js
+++ backend/routes/report.route.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { exportTaskReport } from "../controller/report.controller.js"
+import { exportTaskReport, exportUsersReport } from "../controller/report.controller.js"
 
 const router = express.Router()
 router.get("/export/tasks", verifyToken , adminOnly, exportTaskReport)
+router.get("/export/users", verifyToken , adminOnly, exportUsersReport)
 export default router
